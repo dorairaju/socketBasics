09d98debf67577ed40f3af9ca59355aefdb436ff
Dorairaju <dorairaj512@gmail.com>
2016-10-26 01:14:03 -0500

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,10 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to socket.io server!');
 });
+
+//below code is to listen the code which is sent by the server to socket server
+socket.on('message', function (message) {
+
+	console.log("New message: " + message.text)
+
+});
